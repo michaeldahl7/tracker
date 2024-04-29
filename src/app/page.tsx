87a0086c716a0a834f573dc1b5c1d6d2f834cccb
Michael Dahl <michaeldahl7@gmail.com>
2024-04-29 14:35:11 -0700

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 // import Link from "next/link";
+// import db from "~/server/db";
+// import { db } from "@vercel/postgres";
 
-export default function HomePage() {
+import { db } from "~/server/db";
+
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center ">
       Hello, Trackers is in progress!
+      <button></button>
     </main>
   );
 }
